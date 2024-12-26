47dfc5a54eb9534063328bd90c0c90b1f00e94c7
ZaHooman-FTC <147757009+ZaHooman-FTC@users.noreply.github.com>
2024-12-26 17:27:04 -0500

    Create .java

---
+++ .java
@@ -0,0 +1,5 @@
+echo "public class Main { public static void main(String[] args) { System.out.println(\"Hello, world!\"); }}" > Main.java
+
+git add Main.java
+git commit -m "Add Main.java to trigger language detection"
+git push origin main
